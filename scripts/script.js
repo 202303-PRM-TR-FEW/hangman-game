ff0e035c605ac01a3d7c2ace97ee59a7cb88edd0
Alper Yazagan <alperyazagan@gmail.com>
2023-05-11 22:35:29 +0300

    better

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,11 @@ const generateBtn = document.querySelector("#generatebtn");
 generateBtn.addEventListener("click", loadTheWord);
 
 //cubuks DOM
-const cubuks = document.querySelector("#cubuks");
-cubuks.innerHTML = "-";
+//const cubuks = document.getElementById("cubuks");
+
+//Man display DOM
+//const mandrawing = document.getElementsByClassName("gallows").lastChild;
+//mandrawing.style.display = "none";
 
 function loadTheWord() {
   return fetch(`https://random-word-api.herokuapp.com/word?number=1`)
@@ -23,9 +26,9 @@ function loadTheWord() {
     })
     .catch((err) => console.error(err));
 }
-console.log(word);
-cubuks.innerHTML = [...word].map((x) => "_");
-//Aymans part
+//[...word].map((x) => "_");
+
+//Aymans alphabet part
 function buttons() {
   const alphabetButtons = document.getElementById("buttons");
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -43,18 +46,20 @@ buttons();
 
 function ButtonClick(letter) {
   let lowercaseLetter = letter.toLowerCase();
-  console.log(word);
   if (word.includes(lowercaseLetter)) {
     console.log("letter is in");
     //show letters on cubuks
   } else {
     if (lives == 1) {
       //game over
+      console.log("game over");
     } else {
-      lives = lives--;
+      lives--;
+      currentlives.innerHTML = `You have ${lives} lives left `;
+      document.getElementById(`arm-${lives}`).style.display = "block";
       //show one more man part
       //function
-      console.log("letter not here")`kol-${lives}`;
+      // console.log("letter not here")`kol-${lives}`;
     }
   }
 }
