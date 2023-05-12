75acb110cf431a36b134444d89dbb284e8b3aec5
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-05-12 19:51:47 +0300

    second step

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
+const word = []
 async function getRandomWord() {
     const resp = await fetch("https://random-word-api.herokuapp.com/word?number=1")
     const array = await resp.json()
-    const word = array[0]
+    word.push(array[0])
     console.log(word)
     wordLength(word)
     replacLitters(word)
@@ -11,26 +12,39 @@ function wordLength(word) {
     let length = word.length
     console.log(length)
 }
+
+const blank = document.querySelector("#blanks")
+const pargraph = document.createElement("p")
+const blankInd=[]
 function replacLitters(word) {
-    const blank = document.querySelector("#blanks")
-    const pargraph = document.createElement("p")
-    for (let i of word) {
-        pargraph.textContent += ' _ '
-        // console.log(i)
-        blank.appendChild(pargraph)
+
+    for (let i = 0; i < word[0].length; i++) {
+        
+        
+        blankInd[i] =' _ '
     }
-
+    pargraph.textContent += blankInd.join('')
+        blank.appendChild(pargraph)
 }
-
+console.log(blankInd)
 const btns = document.querySelectorAll(".btn")
 btns.forEach(btn => {
-    btn.addEventListener("click",(e) => {
+    btn.addEventListener("click", (e) => {
         let value = btn.getAttribute('value')
-        console.log(value)
         checkletters(value)
     })
 })
-function checkletters(value){
-console.log(" value " + value)
+
+function checkletters(value) {
+    let letter = word[0]
+    let indices = [];
+    for (let i = 0; i < letter.length; i++) {
+        if (letter[i] === value) {
+            indices.push(i);
+            blankInd[i] = value
+            pargraph.textContent = blankInd.join('')
+    
+        }
+    }
 }
 getRandomWord()
